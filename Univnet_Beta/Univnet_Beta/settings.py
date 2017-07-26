cf79e8380eb5e1b7aba60c1b09061cd8dd0dacdc
Ayan Sinha Mahapatra <ayansmahapatra@gamil.com>
2017-07-27 00:25:12 +0530

    Minor Error Fixes is Models and Settings.py

--- Univnet_Beta/Univnet_Beta/settings.py
+++ Univnet_Beta/Univnet_Beta/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'Users.apps.UsersConfig'
+    'Users.apps.UsersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- Univnet_Beta/Users/models.py
+++ Univnet_Beta/Users/models.py
@@ -1,5 +1,27 @@
 from django.db import models
 
+# Like B.E. or B.Tech or M.Tech
+class course_in(models.Model):
+    course_name = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.course_name
+
+#Basically What they work on/ want to work/want to find collaborations on
+class interests(models.Model):
+    interest_name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.interest_name
+
+
+ # Their Department Of Study
+class departments(models.Model):
+    dept_name = models.CharField(max_length = 30)
+
+    def __str__(self):
+        return self.dept_name
+
 class student(models.Model):
     username_roll=models.IntegerField(max_length=15)
     email_id=models.CharField(max_length=30)
@@ -10,11 +32,10 @@ class student(models.Model):
     image_url=models.CharField(max_length=1000)
     cv_url=models.CharField(max_length=1000)
     year_of_study=models.IntegerField(max_length=1)
-    course_name=models.ForeignKey(courses, on_delete=models.CASCADE)
+    course_name=models.ForeignKey(course_in,on_delete=models.CASCADE)
 
     dept_stud=models.ForeignKey(departments,on_delete=models.CASCADE)
     work_field_main = models.ForeignKey(interests, on_delete=models.CASCADE)
-    work_field_sub = models.ForeignKey(interests, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.username_roll
@@ -34,28 +55,12 @@ class alumni(models.Model):
 
     dept_alum = models.ForeignKey(departments, on_delete = models.CASCADE)
     work_field_main = models.ForeignKey(interests, on_delete = models.CASCADE)
-    work_field_sub = models.ForeignKey(interests, on_delete = models.CASCADE)
 
     def __str__(self):
         return self.user_name
 
-#Basically What they work on/ want to work/want to find collaborations on
-class interests(models.Model):
-    interest_name = models.CharField(max_length = 30)
 
-    def __str__(self):
-        return self.interest_name
 
- # Their Department Of Study
-class departments(models.Model):
-    dept_name = models.CharField(max_length = 30)
 
-    def __str__(self):
-        return self.dept_name
 
-# Like B.E. or B.Tech or M.Tech
-class courses(models.Model):
-    course_name = models.CharField(max_length=10)
 
-    def __str__(self):
-        return self.course_name
