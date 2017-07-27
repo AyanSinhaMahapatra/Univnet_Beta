2feeb849f48a455e0b368d47348f4bfa105d1e90
Ayan Sinha Mahapatra <ayansmahapatra@gamil.com>
2017-07-28 00:28:57 +0530

    Minor Error Fixes

--- Univnet_Beta/Users/admin.py
+++ Univnet_Beta/Users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import course_in, departments, interests, alumni, student
+
+admin.site.register(course_in)
+admin.site.register(departments)
+admin.site.register(interests)
+admin.site.register(alumni)
+admin.site.register(student)
+
 
 # Register your models here.
--- Univnet_Beta/Users/models.py
+++ Univnet_Beta/Users/models.py
@@ -2,14 +2,14 @@ from django.db import models
 
 # Like B.E. or B.Tech or M.Tech
 class course_in(models.Model):
-    course_name = models.CharField(max_length=10)
+    course_name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.course_name
 
 #Basically What they work on/ want to work/want to find collaborations on
 class interests(models.Model):
-    interest_name = models.CharField(max_length = 30)
+    interest_name = models.CharField(max_length = 100)
 
     def __str__(self):
         return self.interest_name
@@ -17,28 +17,28 @@ class interests(models.Model):
 
  # Their Department Of Study
 class departments(models.Model):
-    dept_name = models.CharField(max_length = 30)
+    dept_name = models.CharField(max_length = 100)
 
     def __str__(self):
         return self.dept_name
 
 class student(models.Model):
-    username_roll=models.IntegerField(max_length=15)
-    email_id=models.CharField(max_length=30)
-    password=models.CharField(max_length=20)
-    name=models.CharField(max_length=30)
-    phone_no=models.CharField(max_length=15)
-    bio=models.CharField(max_length=200)
+    username_roll=models.IntegerField()
+    email_id=models.CharField(max_length=100)
+    password=models.CharField(max_length=40)
+    name=models.CharField(max_length=50)
+    phone_no=models.IntegerField()
+    bio=models.CharField(max_length=600)
     image_url=models.CharField(max_length=1000)
     cv_url=models.CharField(max_length=1000)
-    year_of_study=models.IntegerField(max_length=1)
+    year_of_study=models.IntegerField()
     course_name=models.ForeignKey(course_in,on_delete=models.CASCADE)
 
     dept_stud=models.ForeignKey(departments,on_delete=models.CASCADE)
     work_field_main = models.ForeignKey(interests, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.username_roll
+        return f"Roll No: {self.username_roll}"
 
 
 class alumni(models.Model):
@@ -46,7 +46,7 @@ class alumni(models.Model):
     email_id = models.CharField(max_length = 30)
     password = models.CharField(max_length = 20)
     name = models.CharField(max_length = 30)
-    phone_no = models.CharField(max_length = 15)
+    phone_no = models.IntegerField()
     bio = models.CharField(max_length = 200)
     image_url = models.CharField(max_length = 1000)
     cv_url = models.CharField(max_length = 1000)
