9a395598ccc885d4b806e2026b31a0912d35a154
Sayan Goswami <goswami.sayan47@gmail.com>
2017-07-28 12:59:23 +0530

    Frontend + Backend = Magic !

--- Univnet_Beta/Univnet_Beta/settings.py
+++ Univnet_Beta/Univnet_Beta/settings.py
@@ -114,9 +114,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
 # For the User Uploads
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MEDIA_URL = '/media/'
-
--- Univnet_Beta/Users/views.py
+++ Univnet_Beta/Users/views.py
@@ -6,10 +6,10 @@ from .models import Alumni, Student
 def details_stud(request, username_roll_request):
     stud_var = get_object_or_404(Student, username_roll=username_roll_request)
 
-    return render(request, 'Users/details_student.html', {'student': stud_var})
+    return render(request, 'student_profile.html', {'student': stud_var})
 
 
 def details_alumni(request, username_roll_request):
     alumni_var = get_object_or_404(Alumni, user_name=username_roll_request)
 
-    return render(request, 'Users/details_alumni.html', {'alumni': alumni_var})
+    return render(request, 'alumni_profile.html', {'alumni': alumni_var})
