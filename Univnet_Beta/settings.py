df2acc5d3f462c4f5d3ef45ba53f6711dddc3423
Sayan Goswami <goswami.sayan47@gmail.com>
2017-07-29 13:05:36 +0530

    Fixes

--- Univnet_Beta/settings.py
+++ Univnet_Beta/settings.py
@@ -134,6 +134,3 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # Update database configuration with $DATABASE_URL.
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
-
-
-print(DATABASES)
