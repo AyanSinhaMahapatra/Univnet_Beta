2096d927de8bca5ae26eabec2b2f8466ec3a2d26
Sayan Goswami <goswami.sayan47@gmail.com>
2017-07-29 14:10:36 +0530

    Changed page structure

--- Univnet_Beta/urls.py
+++ Univnet_Beta/urls.py
@@ -20,7 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^home/', include('Users.urls')),
+    url(r'', include('Users.urls')),
 ]
 
 handler404 = 'Univnet_Beta.views.handler400'
--- Users/urls.py
+++ Users/urls.py
@@ -4,14 +4,14 @@ from . import views
 app_name = 'Users'
 
 urlpatterns = [
-    # /home/
+    # /
 
     # home_page for Students
-    # /home/student/<username_roll>/
+    # /student/<username_roll>/
     url(r'^student/(?P<username_roll_request>[0-9]+)/$', views.details_stud, name='details_stud'),
 
     # home_page for Alumni
-    # /home/alumni/<username>/
+    # /alumni/<username>/
     url(r'^alumni/(?P<username_roll_request>[a-z]+[0-9]+)/$', views.details_alumni, name='details_alumni'),
 
 ]
