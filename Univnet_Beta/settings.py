a02ecb7d3556733ab9f7467b80b8762b15720e1f
Ayan Sinha Mahapatra <ayansmahapatra@gamil.com>
2017-08-02 12:25:18 +0530

    AWS Shifting Complete :rocket:

--- Univnet_Beta/settings.py
+++ Univnet_Beta/settings.py
@@ -131,7 +131,7 @@ AWS_HEADERS = {
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') #TODO Sayan check this saying unresolved reference :3
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
@@ -141,7 +141,7 @@ STATICFILES_DIRS = [
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_URL = '/media/'
+#MEDIA_URL = '/media/'
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
@@ -155,7 +155,7 @@ AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME', default='')
 AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID', default='')
 AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY', default='')
 
-
+STATICFILES_LOCATION = 'static'
 
 # Tell django-storages that when coming up with the URL for an item in S3 storage, keep
 # it simple - just use this domain plus the path. (If this isn't set, things get complicated).
@@ -171,5 +171,8 @@ STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
 # you run `collectstatic`).
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
+MEDIAFILES_LOCATION= 'media'
+MEDIA_URL = "https://%s/%s/" % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+DEFAULT_FILE_STORAGE = 'Users.custom_storages.MediaStorage'
 
 # TODO:-  Only media files to be served from AWS !
